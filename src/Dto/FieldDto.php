98f5f16a1515daf79d9c8059f0e0301c59db705d
Victor Bocharsky <bocharsky.bw@gmail.com>
2020-10-30 23:46:29 +0200

    Do not set default template name for FieldDto

--- src/Dto/FieldDto.php
+++ src/Dto/FieldDto.php
@@ -40,7 +40,6 @@ final class FieldDto
     public function __construct()
     {
         $this->cssClass = '';
-        $this->templateName = 'crud/field/text';
         $this->assets = new AssetsDto();
         $this->translationParameters = [];
         $this->formTypeOptions = KeyValueStore::new();
